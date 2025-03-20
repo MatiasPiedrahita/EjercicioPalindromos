6461cac092f5f12f91179204988f5951bc3b0462
Matias Piedrahita Orozco <matias.piedrahita@upb.edu.co>
2025-03-20 09:05:48 -0500

    Update IdentificadorPalindromo.py

--- IdentificadorPalindromo.py
+++ IdentificadorPalindromo.py
@@ -1,18 +1,23 @@
 #Identificador palindromo
 #coding = utf-8
+import unicodedata
+def quitar_tildes(frase):
+    return ''.join(c for c in unicodedata.normalize('NFD', frase) if unicodedata.category(c) != 'Mn')
+
 def comprobarPalindormo(frase):
     fraseOriginal = frase
     fraseOriginal = list(fraseOriginal.lower())
     frase = list(frase.lower())
     frase.reverse()
-    frase = [elemento for elemento in frase if elemento not in [" "]]
-    fraseOriginal = [elemento for elemento in fraseOriginal if elemento not in [" "]]
+    frase = [letra for letra in frase if letra not in [" ", ","]]
+    fraseOriginal = [letra for letra in fraseOriginal if letra not in [" ", ","]]
     if fraseOriginal == frase:
         return True
     else:
         return False
 
 frase = input("Ingrese una frase para comprobar si es un palindromo: \n")
+frase = quitar_tildes(frase)
 esPalindormo = comprobarPalindormo(frase)
 if esPalindormo == True:
     print(f"La frase {frase} se escribe igual de izquierda a derecha y de derecha a izquierda, por lo tanto es un palindromo")
